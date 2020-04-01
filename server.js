1e334e171194e0971c850a2f7cc8471a7172710e
WESTSA <samantha.west@mpac.ca>
2020-03-31 20:53:15 -0400

    testing

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-
+const dir = express.static(__dirname + '/dist');
+console.log(__dirname);
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join( `${dir}/index.html`));
 })
 
 console.log('Console Listenin');
