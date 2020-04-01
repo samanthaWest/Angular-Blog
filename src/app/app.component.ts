6190394385f86b7a999bc1dcb515a87d84415f03
WESTSA <samantha.west@mpac.ca>
2020-04-01 00:54:44 -0400

    Author text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB422 – Assignment 06
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Samantha West Student ID: 128111168 Date: April 1st, 2020
+*
+* Heroku Link: https://webassign-6.herokuapp.com/
+*
+********************************************************************************/ 
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
